8ff26d0d0521ed82f22b24cee57de893fe5ac94d
Anatoliy <t7313@ya.ru>
2022-02-01 06:28:59 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course2collections/Course2CollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2CollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2CollectionsApplication.class, args);
+    }
+
+}
